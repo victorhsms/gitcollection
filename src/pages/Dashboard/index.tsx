ef4814417e2e98dba02ffd988aed3a8cbfc9f038
victorhsmsantos <victorhsmsantos@gmail.com>
2022-05-12 21:18:30 -0300

    Estilizacao do dashboard - pt1

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
-import { Title } from './styles'
+import { Title, Form } from './styles'
+import logo from '../../assets/logo.svg';
 
 export const Dashboard: React.FunctionComponent = () => {
-  return <Title>Dashboard</Title>
+  return (
+    <>
+      <img src={ logo } alt="GitCollection" />
+      <Title>Catálogo de repositórios do Github</Title>
+      <Form>
+        <input placeholder='username/repository_name' />
+        <button type='submit'>Buscar</button>
+      </Form>
+    </>
+  );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,10 +1,46 @@
 import styled from 'styled-components';
 
+import { shade } from 'polished';
+
 export const Title = styled.h1`
   font-size: 48px;
-  text-align: center;
   color: #3a3a3a;
   max-width: 450px;
   line-height: 56px;
   margin-top: 80px;
+`;
+
+export const Form = styled.form`
+  margin-top: 40px;
+  max-width: 700px;
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 2px solid white;
+    border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+    border-right: 0;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    width: 160px;
+    background-color: #04d361;
+    border-radius: 0 5px 5px 0;
+    border: 0;
+    color: white;
+    font-weight: bold;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background-color: ${shade(0.2, '#04d361')};
+    }
+  }
+
 `;
